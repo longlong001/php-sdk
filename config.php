82b50c02ac50c78242842cce19d3db7dcd4ba0e9
longlong001 <2064489716@qq.com>
2013-11-23 19:37:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('APPKEY', 'appkey value');//设置App Key
-define('APPSECRET', 'appsecret value');//设置App Secret
-define('SPACENAME', 'spacename value');//空间名称
+define('APPKEY', 1746138518//设置App Key
+define('APPSECRET', 1a8512f8e8e9a7dcbe88e33bec26c925//设置App Secret
+define('SPACENAME', longlong001//空间名称
